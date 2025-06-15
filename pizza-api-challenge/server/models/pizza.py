fd3ea9186ff6bce14e84b5d17ed683eb8edcf42f
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-06-15 22:12:09 +0300

    added the tablename

--- pizza-api-challenge/server/models/pizza.py
+++ pizza-api-challenge/server/models/pizza.py
@@ -1,6 +1,8 @@
 from server.app import db
 
 class Pizza(db.Model):
+    __tablename__ = "pizza"
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     ingredients = db.Column(db.String, nullable=False)
