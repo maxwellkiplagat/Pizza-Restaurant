82fc87f9aef611d4d18e0228fd906f31c9b5f703
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-06-15 22:05:23 +0300

    added the table name previously forgoten

--- pizza-api-challenge/server/models/restaurant.py
+++ pizza-api-challenge/server/models/restaurant.py
@@ -2,6 +2,9 @@ from server.app import db
 from sqlalchemy.orm import relationship
 
 class Restaurant(db.Model):
+    __tablename__ = "restaurant"
+    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     address = db.Column(db.String, nullable=False)
